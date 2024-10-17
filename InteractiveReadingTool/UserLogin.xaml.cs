1b7033f3be311990a992e6c689cccc096e87e0ff
rogitha <lueckero36@uwosh.edu>
2024-10-16 21:49:20 -0500

    Finished Login Screen

---
+++ InteractiveReadingTool/UserLogin.xaml.cs
@@ -0,0 +1,26 @@
+namespace InteractiveReadingTool;
+
+public partial class UserLogin : ContentPage
+{
+    public UserLogin()
+    {
+        InitializeComponent();
+    }
+
+    // private void LoginButton_Click(object sender, RoutedEventArgs e)
+    // {
+    //     string username = UsernameTextBox.Text;
+    //     string password = PasswordBox.Password;
+
+    //     // For now, just display a message box.
+    //     if (username == "admin" && password == "password") // Example credentials
+    //     {
+    //         MessageBox.Show("Login successful!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+    //     }
+    //     else
+    //     {
+    //         MessageBox.Show("Invalid username or password.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+    //     }
+    // }
+}
+
